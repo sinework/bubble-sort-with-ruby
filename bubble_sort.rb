d667eced9b2ca92274810b87da4a857031dfbae0
sinework <sine.s.amare@gmail.com>
2020-09-17 21:52:32 +0300

    Add: Added the yield block

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,17 @@
 def bubble_sort(nums)
   len = nums.length
   i = 0
-  swap= true
+  swap = true
   while i < len - 1
-    swap=false
+    swap = false
     j = 0
     while j < len - 1
       if nums[j] > nums[j + 1]
         nums[j], nums[j + 1] = nums[j + 1], nums[j]
-        swap=true
+        swap = true
         j += 1
       else
-        
+
         j += 1
 
       end
@@ -19,20 +19,48 @@ def bubble_sort(nums)
     end
 
     i += 1
-    break if swap==false
+    break if swap == false
+
     puts i
   end
- 
+
   print nums
 end
-nums = [1, 5, 6, 3, 2, 4, 7, 8, 9]
+nums = [1, 5, 6, 3, 2, 9, 4, 8, 7]
 bubble_sort(nums)
-
+# /////////////////////////////////////////////////////
 def bubble_sort_by(words)
-  
+  len = words.length
+  i = 0
+  swap = true
+  while i < len - 1
+    swap = false
+    j = 0
+    while j < len - 1
+
+      if yield(words[j], words[j + 1]).positive?
+
+        words[j], words[j + 1] = words[j + 1], words[j]
+
+        swap = true
+
+        j += 1
+
+      else
+        j += 1
+
+      end
+
+    end
+    i += 1
+    break if swap == false
+
+  end
+
+  print words
 end
 
-bubble_sort_by(['hey','hi','hello']){|left,right|
-left.length - right.length
- 
+bubble_sort_by(['microverse', 'is', 'the', 'best', 'techonogy', 'bootcamp', 'in', 'the', 'world']) { |left, right|
+  left.length - right.length
 }
+
