5184599417903d50d5a029c64f72508683e8fc16
sinework <sine.s.amare@gmail.com>
2020-09-17 21:20:16 +0300

    Add: Method definition for bubble_sort_by method was added

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,28 +1,38 @@
 def bubble_sort(nums)
-  len=nums.length
-  i=0
+  len = nums.length
+  i = 0
+  swap= true
+  while i < len - 1
+    swap=false
+    j = 0
+    while j < len - 1
+      if nums[j] > nums[j + 1]
+        nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        swap=true
+        j += 1
+      else
+        
+        j += 1
 
-   while i<len-1 
-   j=0
-      while j<len-1
-             if nums[j]>nums[j+1]
-          nums[j],nums[j+1]=nums[j+1],nums[j]
-          
-           else
-            j+=1
-          
-          end
-
-          
       end
-     
-    i+=1
+
+    end
+
+    i += 1
+    break if swap==false
+    puts i
   end
-  print  nums
+ 
+  print nums
 end
-nums=[1,5,6,7,2,8,3,9,4]
+nums = [1, 5, 6, 3, 2, 4, 7, 8, 9]
 bubble_sort(nums)
 
+def bubble_sort_by(words)
+  
+end
 
-
-
+bubble_sort_by(['hey','hi','hello']){|left,right|
+left.length - right.length
+ 
+}
